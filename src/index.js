66e825efdd8873b567f596b14192a94672e642a5
leongaban <leongaban@gmail.com>
2017-05-19 21:56:35 -0500

    updated package.json

--- src/index.js
+++ src/index.js
@@ -2,23 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from "react-hot-loader";
 import App from './App';
-
 const element = document.getElementById('lifeleveler');
-
-const render = (Component) => {
-	ReactDOM.render(
-	    <AppContainer>
-	    	<Component />
-	    </AppContainer>,
-	    element
-	);
-};
-
-render(App);
-
-// Hot module replacement api
-if (module.hot) {
-  module.hot.accept("./App", () => {
-    render(App);
-  });
-}
+ReactDOM.render(<App />, element);
