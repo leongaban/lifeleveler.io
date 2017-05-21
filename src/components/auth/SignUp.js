6fc109e39b492da6864183f42e6dc9a0345e056e
leongaban <leongaban@gmail.com>
2017-05-20 20:21:15 -0500

    init firebase

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React from 'react'
 import LifeLevelerBanner from '../common/LifeLevelerBanner'
+import firebase from 'firebase'
 
 export class SignUp extends React.Component {
 	constructor(props) {
         super(props)
         this.state = {};
+        console.log('firebase', firebase);
     }
 
 	signup() {
         console.log('%c Signup clicked!', 'background: #393939; color: #bada55');
+
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ...
+        });
     }
 
     componentDidMount() {
