b261714175107f931e806c459c551f2806302579
leongaban <leongaban@gmail.com>
2017-05-21 14:26:12 -0500

    FireBase createUserWithEmailAndPassword working

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import firebase from 'firebase'
-import { not } from 'ramda'
+import { not, equals } from 'ramda'
 import LifeLevelerBanner from '../common/LifeLevelerBanner'
 import InputError from '../common/InputError'
 import { validEmail, validPassword } from '../../factories/utility'
@@ -16,16 +16,46 @@ const config = {
 };
 firebase.initializeApp(config);
 
+const createUser = (state) => {
+    const email = state.email.text;
+    const password = state.pass1.text;
+
+    console.log('createUser', state);
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((res) => {
+            console.log('res', res);
+            firebase.auth().onAuthStateChanged(function(user) {
+                if (user) {
+                    debugger;
+                    console.log('User is signed in.');
+                    console.log('user', user);
+                    // Route user back to Login view
+                    // And show message "Please verify your email then login below!"
+                } else {
+                    console.log('No user is signed in.');
+                }
+            });
+        })
+        .catch(function(error) {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log('errorCode', errorCode);
+            console.log('errorMessage', errorMessage);
+        });
+};
+
 export class SignUp extends React.Component {
 	constructor(props) {
         super(props)
         this.state = {
             email: { text: '', error: false },
             pass1: { text: '', error: false },
-            pass2: { text: '', error: false }
+            pass2: { text: '', error: false, errorMsg: '' }
         };
 
         this.handleBlur = this.handleBlur.bind(this);
+        this.signup = this.signup.bind(this);
     }
 
     componentDidMount() {
@@ -34,14 +64,10 @@ export class SignUp extends React.Component {
     }
 
 	signup() {
-        console.log('%c Signup clicked!', 'background: #393939; color: #bada55');
-
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ...
-        });
+        // console.log('%c Signup clicked!', 'background: #393939; color: #bada55');
+        const pass1 = this.state.pass1.text;
+        const pass2 = this.state.pass2.text;
+        equals(pass1, pass2) ? createUser(this.state) : this.setState({ pass2: { error: true, errorMsg: 'Passwords must match' }});
     }
 
     handleBlur(type) {
@@ -53,28 +79,26 @@ export class SignUp extends React.Component {
             case 'email':
                 not(validEmail(text))
                     ? this.setState({ email: { error: true }})
-                    : this.setState({ email: { error: false }});
+                    : this.setState({ email: { error: false, text }});
                 break;
             case 'pass1':
-                console.log('not(validPassword(text))', not(validPassword(text)));
                 not(validPassword(text))
                     ? this.setState({ pass1: { error: true }})
-                    : this.setState({ pass1: { error: false }});
+                    : this.setState({ pass1: { error: false, text }});
                 break;
             case 'pass2':
-                not(validPassword(text))
-                    ? this.setState({ pass2: { error: true }})
-                    : this.setState({ pass2: { error: false }});
+                if (validPassword(text)) {
+                    this.setState({ pass2: { error: false, text }})
+                }
                 break;
         }
-
-        console.log('this.state', this.state);
     }
 
     render() {
     	const tagline = 'Create a new account to start on the journey of leveing up in life.';
         const emailError = this.state.email.error;
         const pass1Error = this.state.pass1.error;
+        const pass2Error = this.state.pass2.error;
 
         const inputClasser = (type) => {
             switch(type) {
@@ -92,7 +116,7 @@ export class SignUp extends React.Component {
                         <ul>
                             <li>
                                 <InputError on={ emailError } msg={ 'Please enter a valid email.' } />
-                                
+
                                 <input type="text"
                                        id="input-signup-email"
                                        placeholder="email"
@@ -103,21 +127,27 @@ export class SignUp extends React.Component {
                             <li>
                                 <InputError
                                     on={ pass1Error }
-                                    width={ 430 }
-                                    msg={ 'A combination of at least 6 numbers, letters and punctuation like ! and &.' } />
+                                    width={ 270 }
+                                    msg={ 'A combination of at least 6 numbers & letters' } />
 
                                 <input type="password"
                                        id="input-signup-pass1"
                                        placeholder="password"
                                        className={inputClasser('pass1')}
+                                       onChange={() => this.handleBlur('pass1') }
                                        onBlur={() => this.handleBlur('pass1')}/>
                             </li>
                             <li>
+                                <InputError
+                                    on={ pass2Error }
+                                    width={ 140 }
+                                    msg={ this.state.pass2.errorMsg } />
+
                                 <input type="password"
                                        id="input-signup-pass2"
                                        placeholder="confirm password"
-                                       className={inputClasser('pass2')}
-                                       onBlur={() => this.handleBlur('pass2')}/>
+                                       onChange={() => this.handleBlur('pass2') }
+                                       className={inputClasser('pass2')}/>
                             </li>
                             <li>
                                 <button className="btn-orange" onClick={ this.signup }>Sign Up</button>
