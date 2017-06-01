a30f81b75ece313de6d52d9cee74caea7135774b
leongaban <leongaban@gmail.com>
2017-06-01 11:18:52 -0500

    added totaler reduce function in util/utility

--- src/util/utility.js
+++ src/util/utility.js
@@ -1,3 +1,14 @@
 import { compose, isEmpty, not } from 'ramda'
 
+const reducer = (accumulator, item) => accumulator + item;
+
 export const notEmpty = compose(not, isEmpty);
+
+export const totaler = (theArray, initialValue=0) => {
+	const total = theArray.reduce(reducer, initialValue);
+	const average = total / theArray.length;
+	return {
+		total,
+		average
+	}
+};
